6fc8e7de7731c37f0a5c4827093c2db79d239967
Dmitry <dimio@dimio.org>
2024-03-10 16:24:39 +0300

    Init via Spring Initializr

---
+++ src/test/java/org/dimio/reactivespringedu/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dimio.reactivespringedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
